4550b0274ac7e8def5330eab63367babfa4b87db
you zhou <husyzy79@gmail.com>
2019-08-15 21:06:31 +0800

    update wechat group forward

--- README.md
+++ README.md
@@ -10,5 +10,17 @@ IMAP会存所有收到的邮件，需要代码设置为seen
 
 微信登录后如果程序断开，过一段时间后可能会需要在手机上点确认按钮来登录
 
+
+
 # WeChat version1.0 
 如果群收到的消息里，有想要的信息，则在filehelper里提醒
+
+
+
+# WeChat version2.0
+监听listenerID群，匹配keyWord后，转发给forwardID群
+
+数据库
+GroupInfo: GroupID, GroupName
+UserInfo: UserID, UserName
+ForwardRule: UserID(ref UserInfo.UserID), ListenerID(ref GroupInfo.GroupID), ForwardID(ref GroupInfo.GroupID), keyWord
--- WeChat/wechatGroupForward.py
+++ WeChat/wechatGroupForward.py
@@ -6,33 +6,35 @@ import datetime
 
 import MySQLdb
 
-import json
+
 
 # 设置字符串匹配字符串
 def stringMatch(text, searchString):
+    """
+
+    :param text: input text
+    :param searchString: match string
+    :return: a list of matched string
+    """
+
     # searchString = 'rmb|人民币|dollar|美元|美金'
-    m = re.search(searchString, text, flags=re.IGNORECASE)
-    if m:
-        print(m.group())
+    matchedString = re.findall(searchString, text, flags=re.IGNORECASE)
+    if matchedString:
+        print(matchedString)
         print("match success")
-        return m.group()
+        return matchedString
     else:
         return False
 
 
 # 微信提醒
-def wechatNotify(msg, forwardGroups, keywordList):
-
+def wechatNotify(msg, forwardList, keywordList):
     """
-    id
-    User
-    Listener
-    Forward
-    KeyWord
 
-    收到群消息，匹配Listener字段
-    然后匹配Keyword，Keyword存储方式为a|b|c|d
-    匹配成功则转发给Forward中的群
+    :param msg: wechat msg
+    :param forwardList: forward group list
+    :param keywordList: keyword list for each forward group
+    :return: None
     """
 
     nowTime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
@@ -43,10 +45,10 @@ def wechatNotify(msg, forwardGroups, keywordList):
         itchat.send("%s, 有业务了! 发消息人: %s, 来自群: %s, 内容: %s" % (nowTime, msg.ActualNickName, msg.User.NickName, msg.text), toUserName='filehelper')
         # 转发的时候需要匹配msg.User.UserName，不能使用NickName
         # 转发给设置需要转发的群
-        for forwardGroup, keywordSend in zip(forwardGroups, keywordList):
-            forwardGroupInfo = itchat.search_chatrooms(name=forwardGroup)
-            forwardGroupUserName = forwardGroupInfo[0]['UserName']
-            itchat.send("%s。关键字: %s。来自群: %s, 内容: %s" % (nowTime, keywordSend, msg.User.NickName, msg.text), forwardGroupUserName)
+        for forwardGroup, keywordSend in zip(forwardList, keywordList):
+            # forwardGroupInfo = itchat.search_chatrooms(name=forwardGroup)
+            # forwardGroupUserName = forwardGroupInfo[0]['UserName']
+            itchat.send("%s。关键字: %s。来自群: %s, 内容: %s" % (nowTime, keywordSend, msg.User.NickName, msg.text), forwardGroup)
 
         print("forward success")
     except Exception as e:
@@ -58,55 +60,64 @@ def wechatNotify(msg, forwardGroups, keywordList):
 # Override the messages in the group chat.(处理群消息)
 @itchat.msg_register(TEXT, isGroupChat=True)
 def text_reply(msg):
+    """
+    1. 收到群消息
+    2. 首先判断是来自哪个群
+    3. 然后确定需要转发给哪些群
+    4. 判断是否有关键字
+    5. 逐个转发
 
-    # 1. 收到群消息
-    # 2. 首先判断是来自哪个群
-    # 3. 然后确定需要转发给哪些群
-    # 4. 判断是否有关键字
-    # 5. 逐个转发
+    """
 
-    msgFromGroupNickName = msg.User.NickName
-    print("msg receive from " + msgFromGroupNickName)
+    msgFromGroupUserName = msg.User.UserName
+    print("msg receive from " + msgFromGroupUserName)
+    # TODO: 突然出现了msg中没有NickName的情况
+    print(msg.User.NickName)
 
     cursor = db.cursor()
     # 如果收到的Listener的消息
-    sql = "SELECT * FROM wechatRule WHERE Listener = '" + msgFromGroupNickName + "'"
+    # sql = "SELECT * FROM wechatRule WHERE Listener = '" + msgFromGroupNickName + "'"
+    sql = "SELECT * FROM ForwardRule WHERE ListenerID = '" + msgFromGroupUserName + "'"
+
 
     try:
         # 执行sql语句
         cursor.execute(sql)
 
-        # 找出所有Listener是该群的群
-        forwardGroups = []
+        # 找出所有ListenerID是该群的群
+        forwardList = []
         keywordList = []
-        keyword = None
+        matchedString = None
 
         results = cursor.fetchall()
 
         for row in results:
-            User = row[1]
-            Listener = row[2]
-            Forward = row[3]
+            UserID = row[1]
+            ListenerID = row[2]
+            ForwardID = row[3]
             KeyWord = row[4]
 
-            # print(User)
-            # print(Listener)
-            # print(Forward)
-            # print(KeyWord)
 
             # 如果字符串匹配
-            keyword = stringMatch(msg.text, KeyWord)
-            if keyword:
-                forwardGroups.append(Forward)
-                keywordList.append(keyword)
+            matchedString = stringMatch(msg.text, KeyWord)
 
-            keyword = None
+            if matchedString:
+                # 将需要转发的对象和关键字存入forwardLists和keywordList两个list
+                if ForwardID:
+                    forwardList.append(ForwardID)
+                else:
+                    forwardList.append(UserID)
+
+                keywordList.append(matchedString)
+
+            matchedString = None
 
         print(keywordList)
-        print(forwardGroups)
+        print(forwardList)
+
         # 需要转发的群以及关键字
         if keywordList != []:
-            wechatNotify(msg, forwardGroups, keywordList)
+            wechatNotify(msg, forwardList, keywordList)
 
         db.commit()
 
@@ -120,20 +131,11 @@ def text_reply(msg):
 
 
 if __name__ == '__main__':
-    # 相关值初始化，以后改为客户输入，是否需要自动联想
-    # TODO: 数组库存转发规则
     # 搜索的字符串暂定用|隔开
-    # searchString = ''
-    # # 监听群
-    # listenerGroups = ''
-    # # 转发群
-    # forwardGroups = ''
-    # # 转发附加信息，可选
-    # forwardMsg = ''
 
-    # 打开数据库连接
+    # 数据库连接
     # TODO: 上传时记得删密码
-    db = MySQLdb.connect("localhost", "", "", "wechat", charset='utf8')
+    db = MySQLdb.connect("localhost", "", "", "wechatForwardDB", charset='utf8')
 
 
     #登陆的时候使用命令行显示二维码, 程序关闭，一定时间内重新开启也可以不用重新扫码
